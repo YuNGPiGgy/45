aa5286d84a5dd0232918abb9c9f58199963b2292
YuNGPiGgy <yungchu1219@gmail.com>
2018-04-11 00:52:45 -0700

    1

---
+++ Lab1/src/Director.java
@@ -0,0 +1,9 @@
+public class Director
+{
+	/*
+	 String - first name of Director
+	 String – last name of Director
+	 int – birth year of Director
+	 int – number of films the director has directed
+	 */
+}
---
+++ Lab1/src/Movie.java
@@ -0,0 +1,11 @@
+public class Movie
+{
+	/*
+	int - duration of the movie in minutes
+	int - year the movie was released
+	double - price of the movie in USD
+	String - title of the movie
+	Director – an object representing the Director of the movie 
+		(for this assignment, you may assume the movie has only one director).
+	*/
+}
