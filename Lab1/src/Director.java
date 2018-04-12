fa4db9e4169239049581aa615e238c8aa0840642
YuNGPiGgy <yungchu1219@gmail.com>
2018-04-12 13:01:45 -0700

    Director&Movie

--- Lab1/src/Director.java
+++ Lab1/src/Director.java
@@ -6,4 +6,55 @@ public class Director
 	 int – birth year of Director
 	 int – number of films the director has directed
 	 */
+	private String FirstName;
+	private String LastName;
+	private int BirthYear;
+	private int NumOfFilm;
+	
+	public Director()
+	{
+	}
+	public Director(String first, String last, int year, int film)
+	{
+		this.FirstName = first;
+		this.LastName = last;
+		this.BirthYear = year;
+		this.NumOfFilm = film;
+	}
+	
+	//Setters
+	public void setFirstName(String first)
+	{
+		this.FirstName = first;
+	}
+	public void setLastName(String last)
+	{
+		this.LastName = last;
+	}
+	public void setBirthYear(int year)
+	{
+		this.BirthYear = year;
+	}
+	public void setNumOfFilm(int film)
+	{
+		this.NumOfFilm = film;
+	}
+
+	//Getters
+	public String getFirstName()
+	{
+		return this.FirstName;
+	}
+	public String getLastName()
+	{
+		return this.LastName;
+	}
+	public int getBirthYear()
+	{
+		return this.BirthYear;
+	}
+	public int getNumOfFilme()
+	{
+		return this.NumOfFilm;
+	}
 }
--- Lab1/src/Movie.java
+++ Lab1/src/Movie.java
@@ -8,4 +8,66 @@ public class Movie
 	Director – an object representing the Director of the movie 
 		(for this assignment, you may assume the movie has only one director).
 	*/
+	private int Duration;
+	private int ReleasedYear;
+	private double Price;
+	private String Title;
+	private Director Director;
+	
+	public Movie()
+	{
+	}
+	public Movie(int time, int year, double price, String title, Director director)
+	{
+		this.Duration = time;
+		this.ReleasedYear = year;
+		this.Price = price;
+		this.Title = title;
+		this.Director = director;
+	}
+	//Setters
+	public void setDuration(int time)
+	{
+		this.Duration = time;
+	}
+	public void setReleasedYear(int year)
+	{
+		this.ReleasedYear = year;
+	}
+	public void setPrice(double price)
+	{
+		this.Price = price;
+	}
+	public void setTitle(String title)
+	{
+		this.Title = title;
+	}
+	public void setDirector(String first, String last, int birthYear, int films)
+	{
+		this.Director = new Director(first, last, birthYear, films);
+	}
+	//Getters
+	public int getDuration()
+	{
+		return this.Duration;
+	}
+	public int getReleasedYear()
+	{
+		return this.ReleasedYear;
+	}
+	public double getPrice()
+	{
+		return this.Price;
+	}
+	public String getTitle()
+	{
+		return this.Title;
+	}
+	public Director getDirector()
+	{
+		return this.Director;
+	}
+	
+	
+	
 }
