e14a4f79dd53ea172e09f861247abc7ff6f22c98
YuNGPiGgy <yungchu1219@gmail.com>
2018-04-12 15:35:58 -0700

    fixed

--- Lab1/src/Lab1.java
+++ Lab1/src/Lab1.java
@@ -9,7 +9,7 @@ public class Lab1
 		Movie HarryPotter5 = new Movie(138, 2007, 10.99, "Harry Potter and the Order of the Phoenix", new Director("David", "Yates", 1963, 27));
 		Movie HarryPotter6 = new Movie(153, 2009, 10.99, "Harry Potter and the Half-Blood Prince", new Director("David", "Yates", 1963, 27));
 		Movie HarryPotter7_1 = new Movie(146, 2010, 19.99, "Harry Potter and the Deathly Hallows – Part 1", new Director("David", "Yates", 1963, 27));
-		Movie HarryPotter7_2 = new Movie(130, 2011, 19.99, "Harry Potter and the Deathly Hallows – Part 2", new Director(("David", "Yates", 1963, 27));
+		Movie HarryPotter7_2 = new Movie(130, 2011, 19.99, "Harry Potter and the Deathly Hallows – Part 2", new Director("David", "Yates", 1963, 27));
 		
 	}
 }
